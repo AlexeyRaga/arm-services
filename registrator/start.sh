0af35c9cb746aed9518f2eaec7d1c87af174fb22
alexey <araga@arbor.net>
2015-04-19 20:27:32 +1000

    registrator service is not a daemon

--- registrator/start.sh
+++ registrator/start.sh
@@ -2,7 +2,7 @@
 
 public_ip="$(ip ro | awk '/^default/{print $9}')"
 
-docker run -d --name registrator \
+docker run --rm --name registrator \
            -v /var/run/docker.sock:/tmp/docker.sock \
            -h $HOSTNAME \
            alexeyraga/arm-registrator \
